22838f247e496c54cee70add5c1cd60c231a27c9
Resi Respati <resir014@gmail.com>
2022-04-20 14:23:14 +0700

    chore: init prettier package

---
+++ packages/prettier/index.js
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  endOfLine: 'auto',
+};
