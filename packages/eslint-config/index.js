b468f31f75b8d459f55f702b8a00d6334f55bf7c
Nelvson <nelvson1337@gmail.com>
2022-05-09 13:37:42 +0700

    feat: eslint extensions & plugins (#21)
    Co-authored-by: nelvson <nelvson.shine@warungpintar.co>

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -8,9 +8,9 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['eslint-comments'],
+  plugins: ['eslint-comments', '@typescript-eslint'],
   extends: [
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
